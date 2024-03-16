c277753f745c335e775aad81e87d86ea867293ae
nv08 <nyvy.07@gmail.com>
2024-03-16 19:48:16 +0530

    commit

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,5 +1,33 @@
+/* eslint-disable react/prop-types */
+import "./index.css";
+import { CHARACTERS } from "./charactersData";
+
+const Card = ({ image, title, description }) => {
+  return (
+    <div className="card">
+      <img src={image} alt={title} className="card-image" />
+      <div className="card-content" >
+        <h2 className="card-title">{title}</h2>
+        <p className="card-description">{description}</p>
+      </div>
+    </div>
+  );
+};
+
 const Characters = () => {
-  return <div>Characters</div>;
+  const characters = CHARACTERS;
+  return (
+    <div className="characters-container">
+      {characters.map((character, index) => (
+        <Card
+          key={index}
+          image={character.image}
+          title={character.title}
+          description={character.description}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Characters;
---
+++ src/components/charactersData.js
@@ -0,0 +1,79 @@
+export const CHARACTERS = [
+  {
+    image: "https://via.placeholder.com/150",
+    title: "Shiva- The Village Boy",
+    description: "Junglepur ka Shiva",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    title: "The Wise Owl- Ustad Ji",
+    description: "Ustad Ji- The Teacher",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    title: "The Elephant- Dr. Haathi",
+    description: "हम हैं डाक्टर हाथी, हम देते हैं सबको दवा मीठी",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    title: "Arpa- The Orator River",
+    description: "The Orator",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    title: "The Bear- Balloo",
+    description: "हमारा नाम हैं बल्लू, हमारे नाम में लगता है बड़ा ऊ",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    title: "The Lion- King Sharab",
+    description: "हम हैं महाराज शरब, जंगलपुर है हमारी रियासत",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    title: "Lion Kid- Badey",
+    description:
+      "हम हैं बड़े, हम किसी को कुछ नही कहते, फ़िर भी सब हैं हमसे डरते",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    title: "Frog- Daddoo",
+    description: "मैं हूँ डड्डू, मैं दिन भर बस यहाँ से वहाँ फुद्कूँ",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    title: "Cat- Mau",
+    description:
+      "मैं हूँ माऊ बिल्ली,मेरे आते ही मच जाती है खलबली,क्योंकि मैं हूँ सबको बहुत पकाती",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    title: "Rabbit- Tukku",
+    description: "मैं हूँ टुक्कु, अपने बिल में रहूँ और खूब गाजर खाऊँ",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    title: "Tortoise- Pichku",
+    description: "मैं हूँ पिचकू, डर लगते ही अपने खोल में दुबकूँ",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    title: "Parrot- Popat",
+    description: "मैं हूँ पोपट, हरि मिर्च खाकर दिन भर करूँ खटपट",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    title: "Rat- KutKut",
+    description: "मैं हूँ कुटकुट, माऊ को आते देख बिल में जाता घुस",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    title: "Squirrel- GulGul",
+    description: "मैं गुलगुल गिलहरी, सब बोलते मैं हूँ बड़ी चुलबुली",
+  },
+  {
+    image: "https://via.placeholder.com/150",
+    title: "Donkey- Ghonchu",
+    description: "हम हैं घोंचू, सामान उठाते और करते ढेंचू ढेंचू",
+  },
+];
