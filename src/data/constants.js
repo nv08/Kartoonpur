cac94fbedc743318681554d96d2d45d1dc76e7db
nv08 <nyvy.07@gmail.com>
2024-03-21 15:01:12 +0530

    add images

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,15 +1,27 @@
 import owl from "../assets/characters/owl.png";
 import rabbit from "../assets/characters/rabbit.png";
 import elephant from "../assets/characters/elephant.png";
-//import fox from "../assets/characters/fox.png";
 import human from "../assets/characters/human.png";
 import tortoise from "../assets/characters/tortoise.png";
-import lion from "../assets/characters/lion.png";
+import lion_kid from "../assets/characters/lion_kid.png";
 import bear from "../assets/characters/bear.png";
 import donkey from "../assets/characters/donkey.png";
 import frog from "../assets/characters/frog.png";
 import rat from "../assets/characters/rat.png";
 import squirrel from "../assets/characters/squirrel.png";
+import fish from "../assets/characters/fish.png";
+import deer from "../assets/characters/deer.png";
+import horse from "../assets/characters/horse.png";
+import hen from "../assets/characters/hen.png";
+import giraffe from "../assets/characters/giraffe.png";
+import dog from "../assets/characters/dog.png";
+import monkey from "../assets/characters/monkey.png";
+import zebra from "../assets/characters/zebra.png";
+import snake from "../assets/characters/snake.png";
+import buffalo from "../assets/characters/buffalo.png";
+import cow from "../assets/characters/cow.png";
+import lion_big from "../assets/characters/lion_big.png";
+import bee from "../assets/characters/bee.png";
 
 export const VIDEOS_URLS = [
   "https://www.youtube.com/embed/2xq94NuXSP8?si=kbNqkUtZBOlOwdtb",
@@ -45,12 +57,12 @@ export const CHARACTERS = [
     description: "हमारा नाम हैं बल्लू, हमारे नाम में लगता है बड़ा ऊ",
   },
   {
-    image: lion,
+    image: lion_big,
     title: "The Lion- King Sharab",
     description: "हम हैं महाराज शरब, जंगलपुर है हमारी रियासत",
   },
   {
-    image: "/assets/characters/owl.png",
+    image: lion_kid,
     title: "Lion Kid- Badey",
     description:
       "हम हैं बड़े, हम किसी को कुछ नही कहते, फ़िर भी सब हैं हमसे डरते",
@@ -61,7 +73,7 @@ export const CHARACTERS = [
     description: "मैं हूँ डड्डू, मैं दिन भर बस यहाँ से वहाँ फुद्कूँ",
   },
   {
-    image: "/assets/characters/owl.png",
+    image: "url",
     title: "Cat- Mau",
     description:
       "मैं हूँ माऊ बिल्ली,मेरे आते ही मच जाती है खलबली,क्योंकि मैं हूँ सबको बहुत पकाती",
@@ -95,5 +107,89 @@ export const CHARACTERS = [
     image: donkey,
     title: "Donkey- Ghonchu",
     description: "हम हैं घोंचू, सामान उठाते और करते ढेंचू ढेंचू",
+  },
+  {
+    image: fish,
+    title: "Fish- MachMach",
+    description: "मैं मच मच. पानी में करती रहूँ छप छप",
+  },
+  {
+    image: deer,
+    title: "Deer- Chikki",
+    description: "मैं हूँ चिक्की, एक छोटी सी हिरनी",
+  },
+  {
+    image: cow,
+    title: "The Cow- GaGa",
+    description: "मेरा नाम है गागा, सभी करते है मेरी पूजा",
+  },
+  {
+    image: horse,
+    title: "Horse- TugBug",
+    description:
+      "मेरा नाम है टगबग, मेरे पैरों में लगी नाल से आवाज़ आती है खट पट",
+  },
+  {
+    image: "url",
+    title: "The Birds- Tinni & Tooni",
+    description: "हम हैं टिन्नी और टूनि, हम हैं जंगलपुर की फैशन queens",
+  },
+  {
+    image: "url",
+    title: "The Butterfly- Lalli",
+    description: "मेरा नाम हैं लल्ली, मै रंग बिरंगी नाज़ुक सी तितली",
+  },
+  {
+    image: bee,
+    title: "The HoneyBee- Bobby",
+    description:
+      "मैं honeybee, हिंदी में बोले तो मधुमक्खी, सभी बुलाते है मुझे Bobby",
+  },
+  {
+    image: "url",
+    title: "Peacock- Pikoo",
+    description: "मैं हूँ पीकू, अपनी मधुर आवाज़ में करता हूँ कूँ कूँ",
+  },
+  {
+    image: buffalo,
+    title: "Buffaloo- Bhaa",
+    description:
+      "मैं एक मोटा तगड़ा भैंसा, करता हूँ भां भां, इसलिए सबने मेरा नाम रख दिया भा",
+  },
+  {
+    image: hen,
+    title: "Hen- DoDo",
+    description: "मेरा नाम है डोडो, और काम, सुबह सवेरे सबको समय पर उठा दो",
+  },
+  {
+    image: "url",
+    title: "Koyal- KuKu",
+    description:
+      "मैं हूँ कूकू, मैं बस सुरीली आवाज़ में गाउँ और खुले आसमान में उडूं",
+  },
+  {
+    image: giraffe,
+    title: "The Giraffe- Lambu",
+    description: "हमारा नाम है लंबू, हम ऊपर से देख सकते हैं पूरा जंगलपुर",
+  },
+  {
+    image: dog,
+    title: "The Dog- भाऊ",
+    description: "मैं भाऊ, मन करता है बस किसी को काट खाऊँ",
+  },
+  {
+    image: snake,
+    title: "Snake- Hiss",
+    description: "",
+  },
+  {
+    image: zebra,
+    title: "Zebra- Zio Dada",
+    description: "हम हैं ज़ियो दादा, हमारे चमकीले दाँत देखकर सबको मज़ा आ जाता",
+  },
+  {
+    image: monkey,
+    title: "Monkey- Chimpu",
+    description: "मैं हूँ चिमपु, एक डाल से दूसरी डाल तक झूलता रहूँ",
   },
 ];
